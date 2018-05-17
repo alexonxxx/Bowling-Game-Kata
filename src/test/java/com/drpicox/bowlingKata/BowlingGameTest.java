524c607e5ea9b0c7a67435549e555079c1f59139
alexonxxx <alex.ballo0806@gmail.com>
2018-05-17 19:59:08 +0200

    test 2 page 24

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -10,11 +10,26 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest extends TestCase {
+    private Game g;
+
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
+
     public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(0);
+        int n = 20;
+        int pins = 0;
+        for (int i = 0; i < n; i++) {
+            g.roll(pins);
+        }
         assertEquals(0, g.score());
+
+    }
+
+    public void testAllOnes() throws Exception {
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
     }
 
 
