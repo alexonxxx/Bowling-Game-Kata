b149bb48c3fa11593c8628888787eec3a8eba2d7
alexonxxx <alex.ballo0806@gmail.com>
2018-05-17 19:53:45 +0200

    test 1 page 14

---
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -0,0 +1,17 @@
+package com.drpicox.bowlingKata;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class BowlingGameTest extends TestCase {
+    public void testGutterGame() throws Exception {
+        Game g = new Game();
+    }
+
+}
